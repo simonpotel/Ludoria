0a1896618bc316df347336d191b387bb2fb31741
axel_lvn <axeloeven@icloud.com>
2024-12-16 14:47:45 +0100

    build : ajout de draw_game

--- src/render.py
+++ src/render.py
@@ -8,17 +8,18 @@ class Render:
     class Render : gère l'interface graphique du game avec tkinter et les événements de clic sur le canvas
     pour le déplacement des pieces et le déroulement du game.
     """
-    def __init__(self, game):
+    def __init__(self, game, quadrants):
         self.game = game 
+        self.quadrants = quadrants 
         self.root = tk.Tk() #fenetre principale tkinter 
         self.root.title = ("KATARENGA & Co") #titre de la fenêtre 
         self.root.geometry("800x800") #taille de la fenêtre 
         self.canvas_with = 400 #largeur du canvas
         self.canvas_height = 400 #hauteur du canvas
-        self.labal_game_name = tk.label(self.root, text=f'Game :  {self.game_name}', 
+        self.label_game_name = tk.Label(self.root, text=f'Game :  {self.game_name}', 
             font=("Roboto, 20")) #label que le joueur donnera au lancement
-        self.labal_game_name.pack(pady=(10,0)) #margin du label de 10 en haut
-        self.label_instruction = tk.label(self.root, text="Au tour du joueur : ", 
+        self.label_game_name.pack(pady=(10,0)) #margin du label de 10 en haut
+        self.label_instruction = tk.Label(self.root, text="Au tour du joueur : ", 
             font=("Roboto, 15")) #label qui indique le tour du joueur
         self.label_instruction.pack(pady=(10,0)) #margin du label de 10 en haut
         self.label_round_player = tk.Label(self.root, text="Joueur 1", 
@@ -31,10 +32,10 @@ class Render:
 
     """
     def load_images(self):
-        """
-                procédure: charge les images des pièces et les redimensionne pour s'adapter aux cellules du board.
+    
+                #procédure: charge les images des pièces et les redimensionne pour s'adapter aux cellules du board.
 
-        """
+        
         self.images = {}
         size = self.game.board.get_size()
         height_cell = self.canvas_height // size
@@ -63,7 +64,57 @@ class Render:
             self.images[key] = ImageTk.PhotoImage(resized_image)
 
     """
+    def draw_game(self):
+        """
+        procédure: dessine le jeu sur le canvas en fonction des positions des pièces dans le board
+        """
+        self.label_instruction.config(
+            text="Your turn to play:", font=("Helvetica", 15))
+        self.label_round_player.config(
+            text=f"Player {self.game.round_player[0] + 1}")
+        
+        size = self.game.board.get_size()
+        width_cell = self.canvas_width // size
+        height_cell = self.canvas_height // size
+        margin = 10
+        width_border = 2.5
 
+        #boucles qui dessinnent les cells du board
+        for i in range(size):
+            for j in range(size):
+                _, color = self.quadrants[i // (size // 2)][j // (size // 2)]
+                color_map = ["#FF0000", "#00FF00", "#0000FF", "#FFFF00"]  # Red, Green, Blue, Yellow
+                fill_color = color_map[color]
+                x, y = j * width_cell, i * height_cell
+                w, h = (j + 1) * width_cell, (i + 1) * height_cell
+                self.canvas.create_rectangle(x, y, w, h, outline="", fill=fill_color)
+
+
+                if piece is not None:
+                    if piece == 1:
+                        image = self.images[f"tower_player_{player + 1}"]
+                    elif piece == 2:
+                        image = self.images[f"tower_player_{player + 1}"]
+                    image_id = self.canvas.create_image(x + width_cell // 2, y + height_cell // 2, image=image)
+                    self.canvas.tag_bind(image_id, '<Button-1>', lambda _, i=i, j=j: self.game.event_click_piece(i, j))
+
+            for i in range(size + 1):
+                # lines horizontales
+                self.canvas.create_line(width_border, i * height_cell, self.canvas_width,
+                                        i * height_cell)
+                # lines verticales
+                self.canvas.create_line(
+                    i * width_cell, width_border, i * width_cell, self.canvas_height + 0.25)
+
+        # ligne gauche verticale
+            self.canvas.create_line(
+                width_border, width_border, width_border, self.canvas_height)
+        # ligne haute horizontale
+            self.canvas.create_line(
+                width_border, width_border, self.canvas_width, width_border)
+
+
+        
 
 
         
