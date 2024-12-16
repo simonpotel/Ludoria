a853be7fa519bd098be8515d1f8adcaf9216e6e4
simon <155122848+simonpotel@users.noreply.github.com>
2024-12-16 10:43:29 +0100

    initialisation des quadrants

---
+++ src/katerenga/game.py
@@ -0,0 +1,13 @@
+from src.board import Board
+from src.render import Render
+
+class Game:
+    def __init__(self, game_save, quadrants):
+        self.game_save = game_save
+        #self.load_game()
+        #self.render = Render()
+        #self.board = Board(quadrants)
+
+    #def load_game(self):
+    #    pass
+    
--- src/selector.py
+++ src/selector.py
@@ -2,10 +2,18 @@ import tkinter as tk
 from tkinter import messagebox
 from tkinter import ttk
 import os
+from src.katerenga.game import Game as Katerenga
+
 
 class Selector:
     GAMES = ["katerenga", "congress", "isolation"]
-    
+    QUADRANTS_COLORS = {
+        'Red': 0,
+        'Green': 1,
+        'Blue': 2,
+        'Yellow': 3
+    }
+
     def __init__(self):
         self.root = tk.Tk()
         self.root.title("Smart Games: Selector")
@@ -17,7 +25,8 @@ class Selector:
         self.entry_game_save = ttk.Combobox(self.root)
         self.entry_game_save['values'] = self.get_saved_games()
         self.entry_game_save.pack(pady=10)
-        self.entry_game_save.bind("<<ComboboxSelected>>", self.on_game_save_change)
+        self.entry_game_save.bind(
+            "<<ComboboxSelected>>", self.on_game_save_change)
         self.entry_game_save.bind("<KeyRelease>", self.on_game_save_change)
 
         tk.Label(self.root, text="Select Game:").pack(pady=10)
@@ -26,7 +35,8 @@ class Selector:
         self.game_selection.current(0)
         self.game_selection.pack(pady=10)
 
-        tk.Button(self.root, text="Load Game", command=self.load_game).pack(pady=10)
+        tk.Button(self.root, text="Load Game",
+                  command=self.load_game).pack(pady=10)
 
     def get_saved_games(self):
         saves_dir = 'saves'
@@ -54,7 +64,55 @@ class Selector:
                 return
             if game_save in self.get_saved_games() or selected_game in self.GAMES:
                 self.root.destroy()
-                exit()
+                # Red = 0, Green = 1, Blue = 2, Yellow = 3
+                quadrant_1 = [[(None, self.QUADRANTS_COLORS['Yellow']), (None, self.QUADRANTS_COLORS['Blue']), (None, self.QUADRANTS_COLORS['Green']), (None, self.QUADRANTS_COLORS['Red'])],
+                              [(None, self.QUADRANTS_COLORS['Red']), (None, self.QUADRANTS_COLORS['Green']), (
+                                  None, self.QUADRANTS_COLORS['Yellow']), (None, self.QUADRANTS_COLORS['Yellow'])],
+                              [(None, self.QUADRANTS_COLORS['Green']), (None, self.QUADRANTS_COLORS['Red']), (
+                                  None, self.QUADRANTS_COLORS['Blue']), (None, self.QUADRANTS_COLORS['Blue'])],
+                              [(None, self.QUADRANTS_COLORS['Blue']), (None, self.QUADRANTS_COLORS['Yellow']), (None, self.QUADRANTS_COLORS['Red']), (None, self.QUADRANTS_COLORS['Green'])]]
+
+                quadrant_2 = [[(None, self.QUADRANTS_COLORS['Red']), (None, self.QUADRANTS_COLORS['Yellow']), (None, self.QUADRANTS_COLORS['Blue']), (None, self.QUADRANTS_COLORS['Green'])],
+                              [(None, self.QUADRANTS_COLORS['Yellow']), (None, self.QUADRANTS_COLORS['Blue']), (
+                                  None, self.QUADRANTS_COLORS['Green']), (None, self.QUADRANTS_COLORS['Red'])],
+                              [(None, self.QUADRANTS_COLORS['Red']), (None, self.QUADRANTS_COLORS['Green']), (
+                                  None, self.QUADRANTS_COLORS['Yellow']), (None, self.QUADRANTS_COLORS['Blue'])],
+                              [(None, self.QUADRANTS_COLORS['Blue']), (None, self.QUADRANTS_COLORS['Red']), (None, self.QUADRANTS_COLORS['Yellow']), (None, self.QUADRANTS_COLORS['Green'])]]
+
+                quadrant_3 = [[(None, self.QUADRANTS_COLORS['Green']), (None, self.QUADRANTS_COLORS['Blue']), (None, self.QUADRANTS_COLORS['Yellow']), (None, self.QUADRANTS_COLORS['Red'])],
+                              [(None, self.QUADRANTS_COLORS['Yellow']), (None, self.QUADRANTS_COLORS['Red']), (
+                                  None, self.QUADRANTS_COLORS['Green']), (None, self.QUADRANTS_COLORS['Blue'])],
+                              [(None, self.QUADRANTS_COLORS['Red']), (None, self.QUADRANTS_COLORS['Yellow']), (
+                                  None, self.QUADRANTS_COLORS['Blue']), (None, self.QUADRANTS_COLORS['Green'])],
+                              [(None, self.QUADRANTS_COLORS['Green']), (None, self.QUADRANTS_COLORS['Blue']), (None, self.QUADRANTS_COLORS['Red']), (None, self.QUADRANTS_COLORS['Yellow'])]]
+
+                quadrant_4 = [[(None, self.QUADRANTS_COLORS['Yellow']), (None, self.QUADRANTS_COLORS['Red']), (None, self.QUADRANTS_COLORS['Blue']), (None, self.QUADRANTS_COLORS['Green'])],
+                              [(None, self.QUADRANTS_COLORS['Blue']), (None, self.QUADRANTS_COLORS['Yellow']), (
+                                  None, self.QUADRANTS_COLORS['Green']), (None, self.QUADRANTS_COLORS['Red'])],
+                              [(None, self.QUADRANTS_COLORS['Green']), (None, self.QUADRANTS_COLORS['Blue']), (
+                                  None, self.QUADRANTS_COLORS['Red']), (None, self.QUADRANTS_COLORS['Yellow'])],
+                              [(None, self.QUADRANTS_COLORS['Red']), (None, self.QUADRANTS_COLORS['Green']), (None, self.QUADRANTS_COLORS['Yellow']), (None, self.QUADRANTS_COLORS['Blue'])]]
+
+                quadrant_5 = [[(None, self.QUADRANTS_COLORS['Blue']), (None, self.QUADRANTS_COLORS['Green']), (None, self.QUADRANTS_COLORS['Red']), (None, self.QUADRANTS_COLORS['Yellow'])],
+                              [(None, self.QUADRANTS_COLORS['Red']), (None, self.QUADRANTS_COLORS['Yellow']), (
+                                  None, self.QUADRANTS_COLORS['Blue']), (None, self.QUADRANTS_COLORS['Green'])],
+                              [(None, self.QUADRANTS_COLORS['Green']), (None, self.QUADRANTS_COLORS['Red']), (
+                                  None, self.QUADRANTS_COLORS['Yellow']), (None, self.QUADRANTS_COLORS['Blue'])],
+                              [(None, self.QUADRANTS_COLORS['Yellow']), (None, self.QUADRANTS_COLORS['Blue']), (None, self.QUADRANTS_COLORS['Green']), (None, self.QUADRANTS_COLORS['Red'])]]
+
+                quadrant_6 = [[(None, self.QUADRANTS_COLORS['Green']), (None, self.QUADRANTS_COLORS['Red']), (None, self.QUADRANTS_COLORS['Yellow']), (None, self.QUADRANTS_COLORS['Blue'])],
+                              [(None, self.QUADRANTS_COLORS['Yellow']), (None, self.QUADRANTS_COLORS['Blue']), (
+                                  None, self.QUADRANTS_COLORS['Red']), (None, self.QUADRANTS_COLORS['Green'])],
+                              [(None, self.QUADRANTS_COLORS['Blue']), (None, self.QUADRANTS_COLORS['Green']), (
+                                  None, self.QUADRANTS_COLORS['Yellow']), (None, self.QUADRANTS_COLORS['Red'])],
+                              [(None, self.QUADRANTS_COLORS['Red']), (None, self.QUADRANTS_COLORS['Yellow']), (None, self.QUADRANTS_COLORS['Blue']), (None, self.QUADRANTS_COLORS['Green'])]]
+
+                match selected_game:
+                    case "katerenga":
+                        Katerenga(game_save, [quadrant_1, quadrant_2, quadrant_3, quadrant_4])
+                    case Else:
+                        messagebox.showerror("Error", "Game not defined.")
+                        exit()
                 self.ask_replay()
             else:
                 messagebox.showerror("Error", "Please select a valid game.")
@@ -62,6 +120,7 @@ class Selector:
             messagebox.showerror("Error", "Please enter a correct value.")
 
     def ask_replay(self):
-        replay = messagebox.askyesno("Smart Games: Selector", "Do you want to replay a new game ?")
+        replay = messagebox.askyesno(
+            "Smart Games: Selector", "Do you want to replay a new game ?")
         if replay:
             self.__init__()
