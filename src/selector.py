51e3bf1dc456652b48b7f34143577f8a988066bc
simon <155122848+simonpotel@users.noreply.github.com>
2024-12-16 09:52:21 +0100

    selector de saves et sélection du jeu

---
+++ src/selector.py
@@ -0,0 +1,67 @@
+import tkinter as tk
+from tkinter import messagebox
+from tkinter import ttk
+import os
+
+class Selector:
+    GAMES = ["katerenga", "congress", "isolation"]
+    
+    def __init__(self):
+        self.root = tk.Tk()
+        self.root.title("Smart Games: Selector")
+        self.setup_initial_ui()
+        self.root.mainloop()
+
+    def setup_initial_ui(self):
+        tk.Label(self.root, text="Game Name:").pack(pady=10)
+        self.entry_game_save = ttk.Combobox(self.root)
+        self.entry_game_save['values'] = self.get_saved_games()
+        self.entry_game_save.pack(pady=10)
+        self.entry_game_save.bind("<<ComboboxSelected>>", self.on_game_save_change)
+        self.entry_game_save.bind("<KeyRelease>", self.on_game_save_change)
+
+        tk.Label(self.root, text="Select Game:").pack(pady=10)
+        self.game_selection = ttk.Combobox(self.root, state="readonly")
+        self.game_selection['values'] = self.GAMES
+        self.game_selection.current(0)
+        self.game_selection.pack(pady=10)
+
+        tk.Button(self.root, text="Load Game", command=self.load_game).pack(pady=10)
+
+    def get_saved_games(self):
+        saves_dir = 'saves'
+        if not os.path.exists(saves_dir):
+            os.makedirs(saves_dir)
+        saved_games = []
+        for files in os.listdir(saves_dir):
+            if files.endswith('.json'):
+                saved_games.append(files.replace('.json', ''))
+        return saved_games
+
+    def on_game_save_change(self, event):
+        game_save = self.entry_game_save.get()
+        if game_save in self.get_saved_games():
+            self.game_selection.pack_forget()
+        else:
+            self.game_selection.pack(pady=10)
+
+    def load_game(self):
+        try:
+            game_save = self.entry_game_save.get()
+            selected_game = self.game_selection.get()
+            if not game_save:
+                messagebox.showerror("Error", "Please enter a game name.")
+                return
+            if game_save in self.get_saved_games() or selected_game in self.GAMES:
+                self.root.destroy()
+                exit()
+                self.ask_replay()
+            else:
+                messagebox.showerror("Error", "Please select a valid game.")
+        except ValueError:
+            messagebox.showerror("Error", "Please enter a correct value.")
+
+    def ask_replay(self):
+        replay = messagebox.askyesno("Smart Games: Selector", "Do you want to replay a new game ?")
+        if replay:
+            self.__init__()
