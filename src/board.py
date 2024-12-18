d7b683c711b3557f196543721f7c708bf36d5593
Noa-trny <noa.tournoy@supinfo.com>
2024-12-18 23:11:33 +0100

    add fonction merge_quadrants (fusion des 4 quadrants)

--- src/board.py
+++ src/board.py
@@ -3,6 +3,7 @@ class Board:
         self.quadrants = quadrants
         self.game_number = game_number
         self.initialize_board()
+        self.merge_quadrants()
 
     def initialize_board(self):
         match self.game_number:
@@ -33,3 +34,15 @@ class Board:
                 self.quadrants[3][2][3][0] = 1
                 self.quadrants[3][3][0][0] = 1
                 self.quadrants[3][3][2][0] = 0
+
+
+    def merge_quadrants(self):
+        merged_board = [[None for _ in range(8)] for _ in range(8)]
+        
+        for i in range(4):
+            for j in range(4):
+                merged_board[i][j] = self.quadrants[0][i][j][0]
+                merged_board[i][j + 4] = self.quadrants[1][i][j][0]
+                merged_board[i + 4][j] = self.quadrants[2][i][j][0]
+                merged_board[i + 4][j + 4] = self.quadrants[3][i][j][0]
+        return merged_board
