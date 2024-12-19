309bc8b1003f671750a7c9f69c856207ac2558ad
Noa <noa.tournoy@supinfo.com>
2024-12-19 22:32:23 +0100

    add: bordure et camp in kataranga

--- src/board.py
+++ src/board.py
@@ -5,6 +5,12 @@ class Board:
         self.initialize_board()
         self.merge_quadrants()
 
+        if game_number == 0:  # Seulement pour Katarenga
+            self.has_outer_cells = True
+            self.outer_cells = self.initialize_outer_cells()
+        else:
+            self.has_outer_cells = False
+
     def initialize_board(self): #prépose les pions sur le plateau
         match self.game_number:
             case 0: # katerenga
@@ -47,3 +53,19 @@ class Board:
                 merged_board[i + 4][j] = self.quadrants[2][i][j][0]
                 merged_board[i + 4][j + 4] = self.quadrants[3][i][j][0]
         return merged_board
+
+    def initialize_outer_cells(self):
+        """
+        Initialise les cases externes du plateau pour Katarenga
+        """
+        # Création d'une grille 10x10 pour les cases externes (8x8 plateau + 1 case de chaque côté)
+        outer_cells = [[None for _ in range(10)] for _ in range(10)]
+        
+        # Marquer les cases camps dans les coins (4 pour la couleur grise)
+        outer_cells[0][0] = [None, 4]  # Coin supérieur gauche
+        outer_cells[0][9] = [None, 4]  # Coin supérieur droit
+        outer_cells[9][0] = [None, 4]  # Coin inférieur gauche
+        outer_cells[9][9] = [None, 4]  # Coin inférieur droit
+        
+        return outer_cells
+
