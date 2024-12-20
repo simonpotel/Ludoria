f30ba82c63fb4ae5096f0e41f8c826d1b5a58287
simon <155122848+simonpotel@users.noreply.github.com>
2024-12-20 10:43:25 +0100

    remove useless code

--- src/board.py
+++ src/board.py
@@ -3,7 +3,6 @@ class Board:
         self.quadrants = quadrants
         self.game_number = game_number
         self.initialize_board()
-        self.merge_quadrants()
 
         if game_number == 0:  # Seulement pour Katarenga
             self.has_outer_cells = True
@@ -11,17 +10,16 @@ class Board:
         else:
             self.has_outer_cells = False
 
-    def initialize_board(self): #prépose les pions sur le plateau
+    def initialize_board(self):  # prépose les pions sur le plateau
         match self.game_number:
-            case 0: # katerenga
+            case 0:  # katerenga
                 for i in range(4):
                     self.quadrants[0][0][i][0] = 0
                     self.quadrants[1][0][i][0] = 0
                     self.quadrants[2][3][i][0] = 1
                     self.quadrants[3][3][i][0] = 1
-                    
 
-            case 1: # congress
+            case 1:  # congress
                 self.quadrants[0][0][1][0] = 0
                 self.quadrants[0][0][3][0] = 1
                 self.quadrants[0][1][0][0] = 1
@@ -42,30 +40,17 @@ class Board:
                 self.quadrants[3][3][0][0] = 1
                 self.quadrants[3][3][2][0] = 0
 
-
-    def merge_quadrants(self):
-        merged_board = [[None for _ in range(8)] for _ in range(8)]
-        
-        for i in range(4):
-            for j in range(4):
-                merged_board[i][j] = self.quadrants[0][i][j][0]
-                merged_board[i][j + 4] = self.quadrants[1][i][j][0]
-                merged_board[i + 4][j] = self.quadrants[2][i][j][0]
-                merged_board[i + 4][j + 4] = self.quadrants[3][i][j][0]
-        return merged_board
-
     def initialize_outer_cells(self):
         """
         Initialise les cases externes du plateau pour Katarenga
         """
         # Création d'une grille 10x10 pour les cases externes (8x8 plateau + 1 case de chaque côté)
         outer_cells = [[None for _ in range(10)] for _ in range(10)]
-        
+
         # Marquer les cases camps dans les coins (4 pour la couleur grise)
         outer_cells[0][0] = [None, 4]  # Coin supérieur gauche
         outer_cells[0][9] = [None, 4]  # Coin supérieur droit
         outer_cells[9][0] = [None, 4]  # Coin inférieur gauche
         outer_cells[9][9] = [None, 4]  # Coin inférieur droit
-        
-        return outer_cells
 
+        return outer_cells
