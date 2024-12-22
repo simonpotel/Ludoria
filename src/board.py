c27e75c78b00a37203934f34bc8a8bcbf22f6b63
simon <155122848+simonpotel@users.noreply.github.com>
2024-12-22 12:38:26 +0100

    feat: board (fusion quadrants)

--- src/board.py
+++ src/board.py
@@ -1,56 +1,69 @@
 class Board:
+    """
+    class Board: représente le plateau de jeu
+    en fonction du jeu (Katerega, Congress, Isolation), les pions sont placés différemment
+    au démarrage.
+
+    structure:
+    quadrants: liste de 4 quadrants
+        chaque quadrant est une liste de 4 lignes
+            chaque ligne est une liste de 4 cases
+                chaque case est une liste de 2 éléments:
+                    (joueur, case)
+                    et joueur (0 = joueur 1 et 1 = joueur 2)
+                    et case = 0 / 1 / 2 / 3 
+                    voir src.render.Render.QUADRANTS_CELLS_COLORS
+    """
+
     def __init__(self, quadrants, game_number):
         self.quadrants = quadrants
         self.game_number = game_number
-        self.initialize_board()
+        self.board = self.get_board()
+        self.setup_board()
+        if self.game_number == 0: # katerenga
+            self.setup_camps()
+        print(self.board)
 
-        if game_number == 0:  # Seulement pour Katarenga
-            self.has_outer_cells = True
-            self.outer_cells = self.initialize_outer_cells()
-        else:
-            self.has_outer_cells = False
-
-    def initialize_board(self):  # prépose les pions sur le plateau
+    def setup_board(self):  
+        """
+        procédure : place les pions sur le plateau de jeu en fonction du jeu pour que la partie puisse démarrer
+        """
         match self.game_number:
             case 0:  # katerenga
-                for i in range(4):
-                    self.quadrants[0][0][i][0] = 0
-                    self.quadrants[1][0][i][0] = 0
-                    self.quadrants[2][3][i][0] = 1
-                    self.quadrants[3][3][i][0] = 1
+                for i in range(8):
+                    self.board[0][i][0] = 0
+                for i in range(8):
+                    self.board[7][i][0] = 1
 
             case 1:  # congress
-                self.quadrants[0][0][1][0] = 0
-                self.quadrants[0][0][3][0] = 1
-                self.quadrants[0][1][0][0] = 1
-                self.quadrants[0][3][0][0] = 0
+                pass # à faire plus tard
 
-                self.quadrants[1][0][0][0] = 0
-                self.quadrants[1][0][2][0] = 1
-                self.quadrants[1][1][3][0] = 0
-                self.quadrants[1][3][3][0] = 1
+            case 2:  # isolation
+                pass  # pas de pions à l'initialisation
 
-                self.quadrants[2][0][0][0] = 1
-                self.quadrants[2][2][0][0] = 0
-                self.quadrants[2][3][1][0] = 1
-                self.quadrants[2][3][3][0] = 0
+    def get_board(self):
+        board = [[[None, None] for _ in range(8)] for _ in range(8)]
 
-                self.quadrants[3][0][3][0] = 0
-                self.quadrants[3][2][3][0] = 1
-                self.quadrants[3][3][0][0] = 1
-                self.quadrants[3][3][2][0] = 0
+        for quadrant in range(4):
+            # i & j = 4 car on a 4 quadrants de 4x4 cases
+            for i in range(4):
+                for j in range(4):
+                    match quadrant: # on gére la disposition car HAUT GAUCHE = Q0, HAUT DROITE = Q1, BAS GAUCHE = Q2, BAS DROITE = Q3
+                        case 0:
+                            board[i][j] = self.quadrants[quadrant][i][j]
+                        case 1:
+                            board[i][j + 4] = self.quadrants[quadrant][i][j]
+                        case 2:
+                            board[i + 4][j] = self.quadrants[quadrant][i][j]
+                        case 3:
+                            board[i + 4][j + 4] = self.quadrants[quadrant][i][j]
 
-    def initialize_outer_cells(self):
-        """
-        Initialise les cases externes du plateau pour Katarenga
-        """
-        # Création d'une grille 10x10 pour les cases externes (8x8 plateau + 1 case de chaque côté)
-        outer_cells = [[None for _ in range(10)] for _ in range(10)]
+        return board
 
-        # Marquer les cases camps dans les coins (4 pour la couleur grise)
-        outer_cells[0][0] = [None, 4]  # Coin supérieur gauche
-        outer_cells[0][9] = [None, 4]  # Coin supérieur droit
-        outer_cells[9][0] = [None, 4]  # Coin inférieur gauche
-        outer_cells[9][9] = [None, 4]  # Coin inférieur droit
-
-        return outer_cells
+    def setup_camps(self):
+        for row in self.board:
+            row.insert(0, [None, None])
+            row.append([None, None])
+        empty_row = [[None, None] for _ in range(10)]
+        self.board.insert(0, empty_row)
+        self.board.append(empty_row)
