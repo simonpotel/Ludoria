1c219a698b09ceadb7c9fadb5f8a83e8c53d7e68
simon <155122848+simonpotel@users.noreply.github.com>
2024-12-22 12:39:05 +0100

    refactor: katerenga game constructor

--- src/katerenga/game.py
+++ src/katerenga/game.py
@@ -5,10 +5,7 @@ from src.render import Render
 class Game:
     def __init__(self, game_save, quadrants):
         self.game_save = game_save
-        # self.load_game()
-        # game numbers: 0 = katerenga, 1 = ...
-        game_number = 0
-        self.board = Board(quadrants, game_number)
+        self.board = Board(quadrants, 0)
         self.render = Render(game=self)
 
     def load_game(self):
