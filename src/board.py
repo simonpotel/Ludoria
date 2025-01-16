da5c40f031ba7e1e62a19b4cc836e6c0cf65f569
Noa-trny <noa.tournoy@supinfo.com>
2025-01-16 11:27:29 +0100

    feat(board) init congress pion

--- src/board.py
+++ src/board.py
@@ -45,11 +45,28 @@ class Board:
                 for i in range(8):
                     self.board[8][i+1][0] = 1
 
-            case 1:  # congress
-                pass  # à faire plus tard
-
-            case 2:  # isolation
+                
+            case 1:  # isolation
                 pass  # pas de pions à l'initialisation
+
+
+            case 2:  # congress
+                self.board[0][1][0] = 1
+                self.board[0][3][0] = 0
+                self.board[0][4][0] = 1
+                self.board[0][6][0] = 0
+                self.board[1][0][0] = 0
+                self.board[1][7][0] = 1
+                self.board[3][0][0] = 1
+                self.board[3][7][0] = 0
+                self.board[4][0][0] = 0
+                self.board[4][7][0] = 1
+                self.board[6][0][0] = 1
+                self.board[6][7][0] = 0
+                self.board[7][6][0] = 1
+                self.board[7][4][0] = 0
+                self.board[7][3][0] = 1
+                self.board[7][1][0] = 0
 
     def get_board(self):
         board = [[[None, None] for _ in range(8)] for _ in range(8)]
