5d0b7729cdb82cfc7c357790a4450cf9cb7b0713
simon <155122848+simonpotel@users.noreply.github.com>
2025-01-16 11:25:53 +0100

    feat(render) info_label

--- src/render.py
+++ src/render.py
@@ -13,17 +13,34 @@ class Render:
         self.board_size = len(game.board.board)  # taille du board dynamique
         self.root = tk.Tk()
         self.root.title("KATARENGA & Co")
-        # taille de la fenêtre
-        self.root.geometry(f"{canvas_size}x{canvas_size}")
-        # canvas pour dessiner le plateau
+
+        main_frame = tk.Frame(self.root)
+        main_frame.pack(padx=10, pady=10)
+
+        self.info_label = tk.Label(
+            main_frame,
+            text="Tour du Joueur 1 - Placez un pion sur une case non menacée",
+            font=('Arial', 12),
+            pady=10
+        )
+        self.info_label.pack()
+
         self.canvas = tk.Canvas(
-            self.root, width=canvas_size, height=canvas_size)
-        self.canvas.pack()  # afficher le canvas
+            main_frame,
+            width=canvas_size,
+            height=canvas_size
+        )
+        self.canvas.pack()
 
         self.load_images()  # charge les images des pièces
         self.render_board()  # rendu du plateau
 
         self.canvas.bind("<Button-1>", self.handle_click)
+
+        self.root.update()
+        window_width = self.root.winfo_reqwidth()
+        window_height = self.root.winfo_reqheight()
+        self.root.geometry(f"{window_width}x{window_height}")
 
     def load_images(self):
         """
@@ -46,6 +63,13 @@ class Render:
             # ajouter l'image redimensionnée dans le dictionnaire self.images
             self.images[f"tower_player_{
                 player}"] = ImageTk.PhotoImage(resized_image)
+
+    def edit_info_label(self, indications):
+        """
+        procédure qui modifie le texte de l'info_label
+        """
+        self.info_label.config(text=indications)
+        self.root.update()
 
     def render_board(self):
         """
