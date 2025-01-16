2666624e6dc16d4d3fb3c2582213fa87875c81e2
simon <155122848+simonpotel@users.noreply.github.com>
2025-01-16 10:33:12 +0100

    feat(game) isolation

--- src/isolation/game.py
+++ src/isolation/game.py
@@ -1,20 +1,35 @@
 from src.board import Board
 from src.render import Render
+from src.captures import is_threatened, has_valid_move
 
 
 class Game:
     def __init__(self, game_save, quadrants):
         self.game_save = game_save
+        self.round_turn = 0  # 0 pour le joueur 1, 1 pour le joueur 2
         self.board = Board(quadrants, 1)
         self.render = Render(game=self)
-
-    def load_game(self):
-        self.render.root.mainloop()
 
     def on_click(self, row, col):
         """
         procédure appelée lorsqu'une cellule est cliquée dans le jeu Isolation
         """
         cell = self.board.board[row][col]
-        print(f"Isolation: Clicked on cell at row {row}, col {col}")
-        print(f"Cell content: {cell}")
+
+        if not has_valid_move(self.board.board, self.round_turn):
+            winner = "Joueur 2" if self.round_turn == 0 else "Joueur 1"
+            print(f"Partie terminée ! {winner} a gagné !")
+
+        if cell[0] is not None:
+            # print("Cette case est déjà occupée")
+            return
+
+        if is_threatened(self.board.board, row, col, self.round_turn):
+            # print("Cette case est menacée par un pion adverse")
+            return
+
+        self.board.board[row][col][0] = self.round_turn
+        self.round_turn = 1 - self.round_turn
+
+    def load_game(self):
+        self.render.root.mainloop()
